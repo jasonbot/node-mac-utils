15944be9654511043c9311a3eef7380bbc871c2a
Jason Scheirer <jason.scheirer@gmail.com>
2025-11-19 16:22:53 -0800

    I'm typing here

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,30 @@
-declare module 'node-mac-utils';
+declare module "node-mac-utils" {
+  export function getRunningInputAudioProcesses(): string[];
+
+  type ProcessInfo = {
+    processName: string;
+    processId?: number;
+    deviceName: string;
+    isActive: boolean;
+  };
+
+  type ResultSuccessWithProcess<T> = {
+    success: true;
+    processes: T[];
+  };
+
+  type ResultErrorWithProcess<T> = {
+    success: false;
+    error: string;
+    code: number;
+    domain: string;
+  };
+
+  export type ResultWithProcesses<T> =
+    | ResultSuccessWithProcess<T>
+    | ResultErrorWithProcess<T>;
+
+  export function getProcessesAccessingMicrophoneWithResult(): ResultWithProcesses<string>;
+  export function getProcessesAccessingMicrophoneDebouncedWithResult(): ResultWithProcesses<string>;
+  export function getProcessesAccessingSpeakersWithResult(): ResultWithProcesses<ProcessInfo>;
+}
