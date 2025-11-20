9b9b747ac179642b50737fb3ee7f353ff3db5041
Jason Scheirer <jason.scheirer@gmail.com>
2025-11-19 16:07:50 -0800

    Bomp

--- pipewire-implementation.js
+++ pipewire-implementation.js
@@ -2,29 +2,47 @@ import * as pipewire from "node-pipewire";
 
 pipewire.createPwThread();
 
+function debouncedResult(fn, delay) {
+  let lastUpdate = 0;
+  let lastResult = undefined;
+
+  return function () {
+    const now = performance.now();
+    if (lastUpdate === 0 || now > lastUpdate + delay) {
+      lastResult = fn();
+      lastUpdate = now;
+    }
+
+    return lastResult;
+  };
+}
+
+function getRunningInputAudioProcesses() {
+  return [];
+}
+
+function getProcessesAccessingMicrophoneWithResult() {
+  return {
+    success: true,
+    error: null,
+    processes: [],
+  };
+}
+
+function getProcessesAccessingSpeakersWithResult() {
+  return {
+    success: true,
+    error: null,
+    processes: [],
+  };
+}
+
 export default {
-  getRunningInputAudioProcesses: () => {
-    return [];
-  },
-  getProcessesAccessingMicrophoneWithResult: () => {
-    return {
-      success: true,
-      error: null,
-      processes: [],
-    };
-  },
-  getProcessesAccessingMicrophoneDebouncedWithResult: () => {
-    return {
-      success: true,
-      error: null,
-      processes: [],
-    };
-  },
-  getProcessesAccessingSpeakersWithResult: () => {
-    return {
-      success: true,
-      error: null,
-      processes: [],
-    };
-  },
+  getRunningInputAudioProcesses,
+  getProcessesAccessingMicrophoneWithResult,
+  getProcessesAccessingMicrophoneDebouncedWithResult: debouncedResult(
+    getProcessesAccessingMicrophoneWithResult,
+    1000,
+  ),
+  getProcessesAccessingSpeakersWithResult,
 };
