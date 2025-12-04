ab0f49af8e45cd7302bc4d0ca5cc64638cc35639
Jason Scheirer <jscheirer@makenotion.com>
2025-12-03 16:22:42 -0800

    More error handling

--- windows/MSIXTools.cpp
+++ windows/MSIXTools.cpp
@@ -5,23 +5,21 @@
 #define IGNORE_HRESULT_ERROR(v) try { v } catch (winrt::hresult_error const&) { }
 
 InstalledApp::InstalledApp(const winrt::Windows::ApplicationModel::Package& app) : AppType("msix") {
-	IGNORE_HRESULT_ERROR({
-		this->AppName = winrt::to_string(app.DisplayName());
-	})
+	IGNORE_HRESULT_ERROR({this->AppName = winrt::to_string(app.DisplayName());});
 	auto id(app.Id());
-	IGNORE_HRESULT_ERROR({
-		this->Id = winrt::to_string(id.FullName());
-	})
-	auto version(id.Version());
-	std::stringstream versionstring;
-	versionstring << version.Major << "." << version.Minor << "." << version.Build << "." << version.Revision;
-	this->Version = versionstring.str();
+	if (id != nullptr) {
+    	IGNORE_HRESULT_ERROR({this->Id = winrt::to_string(id.FullName());});
+    	auto version(id.Version());
+    	std::stringstream versionstring;
+    	versionstring << version.Major << "." << version.Minor << "." << version.Build << "." << version.Revision;
+    	this->Version = versionstring.str();
+	}
 }
 
 InstalledApp::InstalledApp(const winrt::Windows::System::Inventory::InstalledDesktopApp& app) : AppType("desktop") {
-	this->AppName = winrt::to_string(app.DisplayName());
-	this->Id = winrt::to_string(app.Id());
-	this->Version = winrt::to_string(app.DisplayVersion());
+    IGNORE_HRESULT_ERROR({this->AppName = winrt::to_string(app.DisplayName());});
+	IGNORE_HRESULT_ERROR({this->Id = winrt::to_string(app.Id());});
+	IGNORE_HRESULT_ERROR({this->Version = winrt::to_string(app.DisplayVersion());});
 }
 
 std::vector<std::string> ListRunningAppIds() {
