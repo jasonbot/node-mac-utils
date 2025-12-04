ac290882acdb3b8196fb3c5e9ff44a2c58ae86a4
Jason Scheirer <jscheirer@makenotion.com>
2025-12-04 14:51:32 -0800

    Here we go

--- macOS/ImageOCR.h
+++ macOS/ImageOCR.h
@@ -17,6 +17,8 @@ class OCRData {
 public:
   bool success;
   std::vector<OCRObservation> observations;
+  OCRData(const std::vector<OCRObservation> &observations)
+      : success(true), observations(observations) {}
 };
 
 OCRData OCRImageData(const std::vector<uint8_t> &);
